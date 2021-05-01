165087ece8fdc834169c7fdeb3c164fec9cef294
Agusgal <agaldeman@yahoo.coma.r>
2021-05-01 18:31:49 -0300

    added basicLCD and cursor classes

---
+++ src/Cursor.cpp
@@ -0,0 +1,7 @@
+#include "Cursor.h"
+
+cursorPosition::cursorPosition(void)
+{
+	this->row = 0;
+	this->column = 0;
+}
---
+++ src/Cursor.h
@@ -0,0 +1,11 @@
+#pragma once
+
+struct cursorPosition
+{
+public:
+	cursorPosition();
+	
+private:
+	int row;
+	int column;
+};
